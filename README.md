9696b3a16ca950623c342a7c33b3410987d1a47d
Abbasmh <abbasabu3g@gmail.com>
2022-02-05 18:21:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CountryPickerPackage
+
+A description of this package.
---
+++ Sources/CountryPickerPackage/CountryPickerPackage.swift
@@ -0,0 +1,6 @@
+public struct CountryPickerPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
